cf1ec4f1656d638e928cb503c3f02e20eec40e95
Abdul-kalim <abdulkalimkalim2786@gmail.com>
2024-08-13 23:36:16 +0530

    initial commit

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+  // app logos
+  static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.jpg";
+  static const String lightAppLogo = "assets/logos/t-store-splash-logo-white.jpg";
+
+//   social logos
+  static const String google = "assets/logos";
+  static const String facebook = "assets/logos";
+}
